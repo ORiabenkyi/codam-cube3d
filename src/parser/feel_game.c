916086bf12c31df3a060922ae7455ad1e44f3c42
Oleksandr <o.riabenkyi@gmail.com>
2025-11-14 18:36:07 +0100

    next

--- src/parser/feel_game.c
+++ src/parser/feel_game.c
@@ -6,7 +6,7 @@
 /*   By: oriabenk <oriabenk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 12:10:01 by oriabenk          #+#    #+#             */
-/*   Updated: 2025/11/14 18:14:23 by oriabenk         ###   ########.fr       */
+/*   Updated: 2025/11/14 18:35:29 by oriabenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 int	feel_game(t_game *game, char *file)
 {
+	int			fd;
+	char		*line;
+
 	game = malloc(sizeof(t_game));
 	if (!game)
 		return (1);
@@ -24,5 +27,16 @@ int	feel_game(t_game *game, char *file)
 	game->map = malloc(sizeof(char *) * 1);
 	if (!game->map)
 		return (free(game->file), free(game), 1);
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return (1);
+	line = get_next_line(fd);
+	while (line)
+	{
+		free(line);
+		line = get_next_line(fd);
+		game->size_file++;
+	}
+	close(fd);
 	return (0);
 }
