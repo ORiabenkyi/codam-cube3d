57f3eefcda132eb7449dfb4d4fae57a3c40497cc
Oleksandr <o.riabenkyi@gmail.com>
2025-12-02 13:56:14 +0100

    second branch

--- src/mlx/init_mlx.c
+++ src/mlx/init_mlx.c
@@ -6,15 +6,21 @@
 /*   By: oriabenk <oriabenk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 12:10:01 by oriabenk          #+#    #+#             */
-/*   Updated: 2025/12/02 13:26:24 by oriabenk         ###   ########.fr       */
+/*   Updated: 2025/12/02 13:55:43 by oriabenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cube3d.h"
 
+static int 
+
 int	init_mlx(t_game *game)
 {
 	(void)game;
-	// Implementation of mlx initialization
-	return (0); // Return 0 on success, non-zero on failure
+	if (!game || !map)
+		return (1);
+	game->mlx = mlx_init(RESOLUTIONW, RESOLUTIONH, TITEL, 0);
+	if (!game->mlx)
+		return (error_define_mlx());
+	return (0);
 }
